48962052f1b24efd62d47106f047eefb8fc9346f
deogo <diogogpontes@gmail.com>
2024-12-03 09:05:37 -0300

    Terceiro Commit

--- src/main/java/com/prova/prova2_DiogoPontes/Entities/Veiculo.java
+++ src/main/java/com/prova/prova2_DiogoPontes/Entities/Veiculo.java
@@ -6,10 +6,11 @@ import java.io.Serializable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Veiculo implements Serializable { 
-
+    @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
 
     private Long id;
---
+++ src/main/java/com/prova/prova2_DiogoPontes/Repositories/VeiculoRepository.java
@@ -0,0 +1,10 @@
+package com.prova.prova2_DiogoPontes.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prova.prova2_DiogoPontes.Entities.Veiculo;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+    
+    
+}
