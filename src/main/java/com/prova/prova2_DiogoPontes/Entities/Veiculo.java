0cac31759c0ffd443840bd9bd182beaaac7dc32c
deogo <diogogpontes@gmail.com>
2024-12-03 08:45:48 -0300

    Segundo commit

---
+++ src/main/java/com/prova/prova2_DiogoPontes/Entities/Veiculo.java
@@ -0,0 +1,76 @@
+package com.prova.prova2_DiogoPontes.Entities;
+
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Veiculo implements Serializable { 
+
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+
+    private Long id;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String ano;
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public String getAno() {
+        return ano;
+    }
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Veiculo other = (Veiculo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
+
+
