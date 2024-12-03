7e7b73c68880adbbd0bca5387f61f333bb3c954f
deogo <diogogpontes@gmail.com>
2024-12-03 08:37:38 -0300

    Primeiro commit

---
+++ src/main/java/com/prova/prova2_DiogoPontes/Prova2DiogoPontesApplication.java
@@ -0,0 +1,13 @@
+package com.prova.prova2_DiogoPontes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova2DiogoPontesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova2DiogoPontesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prova/prova2_DiogoPontes/Prova2DiogoPontesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova.prova2_DiogoPontes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova2DiogoPontesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
